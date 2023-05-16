a629d82b7652afd62db12c17d70aa8653239ed83
0pilatos0 <Paulvanderlei@hotmail.nl>
2023-05-16 20:01:15 +0200

    added new baseViewModel

---
+++ App/ViewModels/BaseViewModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.ViewModels
+{
+	public class BaseViewModel : INotifyPropertyChanged
+	{
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		private string _title;
+		public string Title { get => _title; set { _title = value; OnPropertyChanged(); } }
+
+		private bool _isLoading;
+		public bool IsLoading { get => _isLoading; set { _isLoading = value; OnPropertyChanged(); } }
+
+		protected bool initialised = false;
+
+		public INavigation Navigation { get; set; }
+
+		protected void OnPropertyChanged([CallerMemberName] string propertyName = "")
+		{
+			var changed = PropertyChanged;
+
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+		}
+
+
+		public void RaisePropertyChanged(params string[] properties)
+		{
+			foreach (var propertyName in properties)
+			{
+				PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+			}
+		}
+	}
+}
