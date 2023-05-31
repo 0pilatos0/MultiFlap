3bdcd880f63b512a532a64b5477fa730fe5f9edd
0pilatos0 <Paulvanderlei@hotmail.nl>
2023-05-31 23:19:47 +0200

    cleaned leaderboardcontroller

--- Server/Controllers/LeaderboardController.cs
+++ Server/Controllers/LeaderboardController.cs
@@ -61,10 +61,6 @@ namespace Server.Controllers
 			return Ok(highscore);
 		}
 
-
-
-		// GET api/leaderboard/{id}
-		[HttpGet("{id}")]
 		public async Task<ActionResult<LeaderboardEntry>> GetLeaderboardEntry(int id)
 		{
 			var leaderboardEntry = await _context.LeaderboardEntries.FindAsync(id);
@@ -101,53 +97,6 @@ namespace Server.Controllers
 			await _context.SaveChangesAsync();
 
 			return CreatedAtAction(nameof(GetLeaderboardEntry), new { id = leaderboardEntry.Id }, leaderboardEntry);
-		}
-
-		// PUT api/leaderboard/{id}
-		[HttpPut("{id}")]
-		public async Task<IActionResult> UpdateLeaderboardEntry(int id, LeaderboardEntry updatedLeaderboardEntry)
-		{
-			if (id != updatedLeaderboardEntry.Id)
-			{
-				return BadRequest();
-			}
-
-			_context.Entry(updatedLeaderboardEntry).State = EntityState.Modified;
-
-			try
-			{
-				await _context.SaveChangesAsync();
-			}
-			catch (DbUpdateConcurrencyException)
-			{
-				if (!LeaderboardEntryExists(id))
-				{
-					return NotFound();
-				}
-				else
-				{
-					throw;
-				}
-			}
-
-			return NoContent();
-		}
-
-		// DELETE api/leaderboard/{id}
-		[HttpDelete("{id}")]
-		public async Task<IActionResult> DeleteLeaderboardEntry(int id)
-		{
-			var leaderboardEntry = await _context.LeaderboardEntries.FindAsync(id);
-
-			if (leaderboardEntry == null)
-			{
-				return NotFound();
-			}
-
-			_context.LeaderboardEntries.Remove(leaderboardEntry);
-			await _context.SaveChangesAsync();
-
-			return NoContent();
 		}
 
 		private bool LeaderboardEntryExists(int id)
