67cb37c00f78acb8736d6fb0701898513ffe25b1
Paul van der Lei <Paul.vanderlei@wics.nl>
2023-06-08 09:18:54 +0200

    introduced some logging

--- Server/Services/GameService.cs
+++ Server/Services/GameService.cs
@@ -10,222 +10,264 @@ using System.Threading.Tasks;
 
 namespace Server.Services
 {
-	public class GameService : IGameService
-	{
-		private readonly ConcurrentDictionary<string, Player> _players = new ConcurrentDictionary<string, Player>();
-		private readonly ConcurrentDictionary<string, Match> _matches = new ConcurrentDictionary<string, Match>();
-		private readonly IHubContext<GameHub> _hubContext;
-		private readonly ILogger<GameService> _logger;
+    public class GameService : IGameService
+    {
+        private readonly ConcurrentDictionary<string, Player> _players =
+            new ConcurrentDictionary<string, Player>();
+        private readonly ConcurrentDictionary<string, Match> _matches =
+            new ConcurrentDictionary<string, Match>();
+        private readonly IHubContext<GameHub> _hubContext;
+        private readonly ILogger<GameService> _logger;
 
-		public GameService(IHubContext<GameHub> hubContext, ILogger<GameService> logger)
-		{
-			_hubContext = hubContext;
-			_logger = logger;
-		}
+        public GameService(IHubContext<GameHub> hubContext, ILogger<GameService> logger)
+        {
+            _hubContext = hubContext;
+            _logger = logger;
+        }
 
-		public Task AddPlayerAsync(string connectionId)
-		{
-			var player = new Player
-			{
-				ConnectionId = connectionId,
-				Y = 0,
-				Score = 0
-			};
+        public Task AddPlayerAsync(string connectionId)
+        {
+            var player = new Player
+            {
+                ConnectionId = connectionId,
+                Y = 0,
+                Score = 0
+            };
 
-			_players.TryAdd(player.ConnectionId, player);
+            _players.TryAdd(player.ConnectionId, player);
 
-			_logger.LogInformation($"Player {player.ConnectionId} connected");
-			_logger.LogInformation($"Player count: {_players.Count}");
+            _logger.LogInformation($"Player {player.ConnectionId} connected");
+            _logger.LogInformation($"Player count: {_players.Count}");
 
-			return UpdateOnlinePlayersCountAsync();
-		}
+            return UpdateOnlinePlayersCountAsync();
+        }
 
-		public async Task RemovePlayerAsync(string connectionId)
-		{
-			if (_players.TryRemove(connectionId, out var player))
-			{
-				if (player.MatchId != null && _matches.TryGetValue(player.MatchId, out var match))
-				{
-					var opponentId = match.Players.FirstOrDefault(p => p.ConnectionId != connectionId)?.ConnectionId;
+        public async Task RemovePlayerAsync(string connectionId)
+        {
+            if (_players.TryRemove(connectionId, out var player))
+            {
+                if (player.MatchId != null && _matches.TryGetValue(player.MatchId, out var match))
+                {
+                    var opponentId = match.Players
+                        .FirstOrDefault(p => p.ConnectionId != connectionId)
+                        ?.ConnectionId;
 
-					_players[opponentId].MatchId = null;
-					_players[opponentId].IsLookingForMatch = false;
-					_players[connectionId].MatchId = null;
-					_players[connectionId].IsLookingForMatch = false;
+                    _players[opponentId].MatchId = null;
+                    _players[opponentId].IsLookingForMatch = false;
+                    _players[connectionId].MatchId = null;
+                    _players[connectionId].IsLookingForMatch = false;
 
-					_matches.TryRemove(match.Id, out var removedMatch);
+                    _matches.TryRemove(match.Id, out var removedMatch);
 
-					await SendToClientAsync(opponentId, "OpponentGameOver", 0);
-					_logger.LogInformation($"Sent OpponentGameOver to {opponentId}");
-				}
-				else
-				{
-					_logger.LogInformation($"Match not found for player {connectionId}");
-				}
-			}
-			else
-			{
-				_logger.LogInformation($"Could not find player in the current player list");
-			}
+                    await SendToClientAsync(opponentId, "OpponentGameOver", 0);
+                    _logger.LogInformation($"Sent OpponentGameOver to {opponentId}");
+                }
+                else
+                {
+                    _logger.LogInformation($"Match not found for player {connectionId}");
+                }
+            }
+            else
+            {
+                _logger.LogInformation($"Could not find player in the current player list");
+            }
 
-			_logger.LogInformation($"Player {connectionId} disconnected");
+            _logger.LogInformation($"Player {connectionId} disconnected");
 
-			await UpdateOnlinePlayersCountAsync();
-		}
+            await UpdateOnlinePlayersCountAsync();
+        }
 
-		public async Task StartMatchmakingAsync(string connectionId)
-		{
-			_logger.LogInformation($"Player {connectionId} started matchmaking");
-			_logger.LogInformation($"Player count: {_players.Count}");
+        public async Task StartMatchmakingAsync(string connectionId)
+        {
+            _logger.LogInformation($"Player {connectionId} started matchmaking");
+            _logger.LogInformation($"Player count: {_players.Count}");
 
-			if (_players.TryGetValue(connectionId, out var player))
-			{
-				player.MatchId = null;
-				player.Y = 0;
-				player.Score = 0;
-				player.IsLookingForMatch = true;
+            //log all players
+            foreach (var plsayer in _players)
+            {
+                _logger.LogInformation(
+                    $"Player {plsayer.Key} is looking for match: {plsayer.Value.IsLookingForMatch}"
+                );
+            }
 
-				var match = FindMatch(player);
+            if (_players.TryGetValue(connectionId, out var player))
+            {
+                player.MatchId = null;
+                player.Y = 0;
+                player.Score = 0;
+                player.IsLookingForMatch = true;
 
-				if (match == null)
-				{
-					_logger.LogInformation($"Matchmaking started for player {player.ConnectionId}");
-					await SendToClientAsync(connectionId, "MatchmakingStarted");
-				}
-				else
-				{
-					_logger.LogInformation($"Match found for player {player.ConnectionId}");
-					match.Players.Add(new PlayerMatchInfo
-					{
-						ConnectionId = player.ConnectionId,
-						Y = player.Y,
-						Score = player.Score
-					});
+                var match = FindMatch(player);
 
-					_matches[match.Id] = match;
+                if (match == null)
+                {
+                    _logger.LogInformation($"Matchmaking started for player {player.ConnectionId}");
+                    await SendToClientAsync(connectionId, "MatchmakingStarted");
+                }
+                else
+                {
+                    _logger.LogInformation($"Match found for player {player.ConnectionId}");
+                    match.Players.Add(
+                        new PlayerMatchInfo
+                        {
+                            ConnectionId = player.ConnectionId,
+                            Y = player.Y,
+                            Score = player.Score
+                        }
+                    );
 
-					var opponentId = match.Players.FirstOrDefault(p => p.ConnectionId != player.ConnectionId)?.ConnectionId;
+                    _matches[match.Id] = match;
 
-					_players[opponentId].MatchId = match.Id;
-					_players[opponentId].IsLookingForMatch = false;
+                    var opponentId = match.Players
+                        .FirstOrDefault(p => p.ConnectionId != player.ConnectionId)
+                        ?.ConnectionId;
 
-					_players[player.ConnectionId].MatchId = match.Id;
-					_players[player.ConnectionId].IsLookingForMatch = false;
+                    _players[opponentId].MatchId = match.Id;
+                    _players[opponentId].IsLookingForMatch = false;
 
-					await SendToClientsAsync(new List<string> { player.ConnectionId, opponentId }, "MatchStarted", match.Players);
-					_logger.LogInformation($"Match {match.Id} started");
-				}
-			}
-			else
-			{
-				_logger.LogInformation($"Player {connectionId} not found");
-			}
-		}
+                    _players[player.ConnectionId].MatchId = match.Id;
+                    _players[player.ConnectionId].IsLookingForMatch = false;
 
-		public async Task GameOverAsync(string connectionId, int score)
-		{
-			_logger.LogInformation($"Received game over from {connectionId}");
+                    await SendToClientsAsync(
+                        new List<string> { player.ConnectionId, opponentId },
+                        "MatchStarted",
+                        match.Players
+                    );
+                    _logger.LogInformation($"Match {match.Id} started");
+                }
+            }
+            else
+            {
+                _logger.LogInformation($"Player {connectionId} not found");
+            }
+        }
 
-			if (string.IsNullOrEmpty(connectionId))
-			{
-				_logger.LogInformation("Connection ID is null or empty");
-				return;
-			}
+        public async Task GameOverAsync(string connectionId, int score)
+        {
+            _logger.LogInformation($"Received game over from {connectionId}");
 
-			if (_players.TryGetValue(connectionId, out var player))
-			{
-				if (player.MatchId != null && _matches.TryGetValue(player.MatchId, out var match))
-				{
-					var opponentId = match.Players.FirstOrDefault(p => p.ConnectionId != connectionId)?.ConnectionId;
+            if (string.IsNullOrEmpty(connectionId))
+            {
+                _logger.LogInformation("Connection ID is null or empty");
+                return;
+            }
 
-					_players[opponentId].MatchId = null;
-					_players[opponentId].IsLookingForMatch = false;
-					_players[connectionId].MatchId = null;
-					_players[connectionId].IsLookingForMatch = false;
+            if (_players.TryGetValue(connectionId, out var player))
+            {
+                if (player.MatchId != null && _matches.TryGetValue(player.MatchId, out var match))
+                {
+                    var opponentId = match.Players
+                        .FirstOrDefault(p => p.ConnectionId != connectionId)
+                        ?.ConnectionId;
 
-					_matches.TryRemove(match.Id, out var removedMatch);
+                    _players[opponentId].MatchId = null;
+                    _players[opponentId].IsLookingForMatch = false;
+                    _players[connectionId].MatchId = null;
+                    _players[connectionId].IsLookingForMatch = false;
 
-					await SendToClientAsync(opponentId, "OpponentGameOver", score);
-					_logger.LogInformation($"Sent OpponentGameOver to {opponentId}");
-				}
-				else
-				{
-					_logger.LogInformation($"Match not found for player {connectionId}");
-				}
-			}
-			else
-			{
-				_logger.LogInformation($"Could not find player in the current player list");
-			}
+                    _matches.TryRemove(match.Id, out var removedMatch);
 
-			_logger.LogInformation($"Player {connectionId} game over");
-		}
+                    await SendToClientAsync(opponentId, "OpponentGameOver", score);
+                    _logger.LogInformation($"Sent OpponentGameOver to {opponentId}");
+                }
+                else
+                {
+                    _logger.LogInformation($"Match not found for player {connectionId}");
+                }
+            }
+            else
+            {
+                _logger.LogInformation($"Could not find player in the current player list");
+            }
 
-		public async Task CancelMatchmakingAsync(string connectionId)
-		{
-			if (_players.TryGetValue(connectionId, out var player))
-			{
-				player.IsLookingForMatch = false;
-				if (player.MatchId != null && _matches.TryGetValue(player.MatchId, out var match))
-				{
-					if (match.Players.Count == 1)
-					{
-						_matches.TryRemove(match.Id, out var removedMatch);
-					}
-					else
-					{
-						match.Players = match.Players.Where(p => p.ConnectionId != connectionId).ToList();
-					}
-				}
+            _logger.LogInformation($"Player {connectionId} game over");
+        }
 
-				await SendToClientAsync(connectionId, "MatchmakingCanceled");
-			}
+        public async Task CancelMatchmakingAsync(string connectionId)
+        {
+            if (_players.TryGetValue(connectionId, out var player))
+            {
+                player.IsLookingForMatch = false;
+                if (player.MatchId != null && _matches.TryGetValue(player.MatchId, out var match))
+                {
+                    if (match.Players.Count == 1)
+                    {
+                        _matches.TryRemove(match.Id, out var removedMatch);
+                    }
+                    else
+                    {
+                        match.Players = match.Players
+                            .Where(p => p.ConnectionId != connectionId)
+                            .ToList();
+                    }
+                }
 
-			_logger.LogInformation($"Player {connectionId} canceled matchmaking");
-		}
+                await SendToClientAsync(connectionId, "MatchmakingCanceled");
+            }
 
-		private async Task UpdateOnlinePlayersCountAsync()
-		{
-			await SendToAllClientsAsync("UpdateOnlinePlayers", _players.Count);
-		}
+            _logger.LogInformation($"Player {connectionId} canceled matchmaking");
+        }
 
-		private async Task SendToClientAsync(string connectionId, string methodName, object? argument = null)
-		{
-			await _hubContext.Clients.Client(connectionId).SendAsync(methodName, argument);
-		}
+        private async Task UpdateOnlinePlayersCountAsync()
+        {
+            await SendToAllClientsAsync("UpdateOnlinePlayers", _players.Count);
+        }
 
-		private async Task SendToClientsAsync(IEnumerable<string> connectionIds, string methodName, object? argument = null)
-		{
-			await _hubContext.Clients.Clients(connectionIds.ToList()).SendAsync(methodName, argument);
-		}
+        private async Task SendToClientAsync(
+            string connectionId,
+            string methodName,
+            object? argument = null
+        )
+        {
+            await _hubContext.Clients.Client(connectionId).SendAsync(methodName, argument);
+        }
 
-		private async Task SendToAllClientsAsync(string methodName, object? argument = null)
-		{
-			await _hubContext.Clients.All.SendAsync(methodName, argument);
-		}
+        private async Task SendToClientsAsync(
+            IEnumerable<string> connectionIds,
+            string methodName,
+            object? argument = null
+        )
+        {
+            await _hubContext.Clients
+                .Clients(connectionIds.ToList())
+                .SendAsync(methodName, argument);
+        }
 
-		private Match FindMatch(Player player)
-		{
-			foreach (var match in _matches.Values)
-			{
-				if (match.Players.Count == 1)
-				{
-					var matchPlayer = match.Players.First();
-					if (Math.Abs(matchPlayer.Y - player.Y) <= 1)
-					{
-						return match;
-					}
-				}
-			}
+        private async Task SendToAllClientsAsync(string methodName, object? argument = null)
+        {
+            await _hubContext.Clients.All.SendAsync(methodName, argument);
+        }
 
-			var newMatch = new Match
-			{
-				Id = Guid.NewGuid().ToString(),
-				Players = new List<PlayerMatchInfo> { new PlayerMatchInfo { ConnectionId = player.ConnectionId, Y = player.Y, Score = player.Score } }
-			};
+        private Match FindMatch(Player player)
+        {
+            foreach (var match in _matches.Values)
+            {
+                if (match.Players.Count == 1)
+                {
+                    var matchPlayer = match.Players.First();
+                    if (Math.Abs(matchPlayer.Y - player.Y) <= 1)
+                    {
+                        return match;
+                    }
+                }
+            }
 
-			_matches.TryAdd(newMatch.Id, newMatch);
-			return newMatch;
-		}
-	}
+            var newMatch = new Match
+            {
+                Id = Guid.NewGuid().ToString(),
+                Players = new List<PlayerMatchInfo>
+                {
+                    new PlayerMatchInfo
+                    {
+                        ConnectionId = player.ConnectionId,
+                        Y = player.Y,
+                        Score = player.Score
+                    }
+                }
+            };
+
+            _matches.TryAdd(newMatch.Id, newMatch);
+            return newMatch;
+        }
+    }
 }
