1ddde7e6345da53ca76afe99568c8dd04aa4ee22
Paul van der Lei <Paul.vanderlei@wics.nl>
2023-06-10 20:02:29 +0200

    expanded database seed

--- Server/MultiFlapDbContext.cs
+++ Server/MultiFlapDbContext.cs
@@ -13,8 +13,16 @@ namespace Server
             modelBuilder
                 .Entity<User>()
                 .HasData(
-                    new User { Id = 1, Email = "admin1@example.com" },
-                    new User { Id = 2, Email = "admin2@example.com" }
+                    new User { Id = 1, Email = "john.doe@example.com" },
+                    new User { Id = 2, Email = "jane.smith@example.com" },
+                    new User { Id = 3, Email = "mark.johnson@example.com" },
+                    new User { Id = 4, Email = "amy.wilson@example.com" },
+                    new User { Id = 5, Email = "michael.brown@example.com" },
+                    new User { Id = 6, Email = "emily.davis@example.com" },
+                    new User { Id = 7, Email = "jacob.miller@example.com" },
+                    new User { Id = 8, Email = "olivia.jones@example.com" },
+                    new User { Id = 9, Email = "william.moore@example.com" },
+                    new User { Id = 10, Email = "sophia.taylor@example.com" }
                 );
 
             modelBuilder
@@ -65,73 +73,72 @@ namespace Server
                     new LeaderboardEntry
                     {
                         Id = 1,
-                        Score = 100,
+                        Score = 700,
                         DateAchieved = DateTime.Now,
                         UserId = 1
                     },
                     new LeaderboardEntry
                     {
                         Id = 2,
-                        Score = 200,
+                        Score = 450,
                         DateAchieved = DateTime.Now,
                         UserId = 2
                     },
-                    // Additional leaderboard entries
                     new LeaderboardEntry
                     {
                         Id = 3,
-                        Score = 150,
+                        Score = 900,
                         DateAchieved = DateTime.Now,
-                        UserId = 1
+                        UserId = 3
                     },
                     new LeaderboardEntry
                     {
                         Id = 4,
-                        Score = 180,
+                        Score = 350,
                         DateAchieved = DateTime.Now,
-                        UserId = 2
+                        UserId = 4
                     },
                     new LeaderboardEntry
                     {
                         Id = 5,
-                        Score = 220,
+                        Score = 600,
                         DateAchieved = DateTime.Now,
-                        UserId = 1
+                        UserId = 5
                     },
                     new LeaderboardEntry
                     {
                         Id = 6,
-                        Score = 300,
+                        Score = 800,
                         DateAchieved = DateTime.Now,
-                        UserId = 2
+                        UserId = 6
                     },
                     new LeaderboardEntry
                     {
                         Id = 7,
                         Score = 250,
                         DateAchieved = DateTime.Now,
-                        UserId = 1
+                        UserId = 7
                     },
                     new LeaderboardEntry
                     {
                         Id = 8,
-                        Score = 190,
+                        Score = 550,
                         DateAchieved = DateTime.Now,
-                        UserId = 2
+                        UserId = 8
                     },
                     new LeaderboardEntry
                     {
                         Id = 9,
-                        Score = 280,
+                        Score = 950,
                         DateAchieved = DateTime.Now,
-                        UserId = 1
+                        UserId = 9
                     },
                     new LeaderboardEntry
                     {
                         Id = 10,
-                        Score = 230,
+                        Score = 400,
                         DateAchieved = DateTime.Now,
-                        UserId = 2
+                        UserId = 10
                     }
                 );
 
