6fd1f261ffff84b6e164a9fa9b7ff77464a68883
Paul van der Lei <Paul.vanderlei@wics.nl>
2023-06-11 12:48:15 +0200

    added new xunit test

---
+++ AppTests/GameCore/GameEngineTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Drawing;
+using App.GameCore;
+using App.GameCore.GameObjects;
+
+namespace AppTests.GameCore
+{
+    public class GameEngineTests
+    {
+        [Fact]
+        public void ResetScore_SetsScoreToZero()
+        {
+            // Arrange
+            var gameEngine = new GameEngine();
+            gameEngine.Score = 10;
+
+            // Act
+            gameEngine.ResetScore();
+
+            // Assert
+            Assert.Equal(0, gameEngine.Score);
+        }
+    }
+}
